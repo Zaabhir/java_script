ec28a8d5bd96d90cc3479f430e63fa3f0dd7faf4
jai prakash <work.zaabhir089@gmail.com>
2026-07-17 17:21:12 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js  as new version 
+
+//alert(3+3) // we are using node.js, not browser
+
+console.log(3
+    +
+  3);
+// code readibility should be high
+  console.log("JP YADAV");
+  
+let name = "jp YAdav"
+let age = 18
+let isLoggedin = false
+let state = null;
+
+//number => 2 to power 53
+// bigint  
+//string => ""
+// boolean => true / false
+// null => standalone value 
+// undefined => standalone value
+// undefined  =>
+//  symbol => unique
+
+
+//object
+console.log(typeof "Jp yadav");
+console.log(typeof "null");// object
+console.log(typeof "undefined");// undefined
