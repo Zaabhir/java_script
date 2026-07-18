3172055210c0e0b6a7d04d91cbcd9e8659cc2741
jai prakash <work.zaabhir089@gmail.com>
2026-07-18 16:22:27 +0000

    03_conversionDataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "Jp YADV"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInumber = Number(score)
+// console.log(typeof valueInumber);
+// console.log(valueInumber);
+
+
+// "33" =>33
+//"33abc" => NaN
+// true  => 1; false => 0
+
+
+// let isLoggedIn = "HI"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true: 0 => false
+// "" = > false
+// "Hi" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
