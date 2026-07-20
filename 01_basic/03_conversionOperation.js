ae094182d0e25343208384645a7778400fa6169f
jai prakash <work.zaabhir089@gmail.com>
2026-07-20 16:09:20 +0000

    conversion

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -23,7 +23,65 @@
 // "Hi" => true
 
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+
+
+// operations
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+
+
+let str1 = "Hello"
+let str2 = " JP"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2); //  "1" is a string.
+//When you use the + operator and one operand is a string, JavaScript converts the other operand to a string and concatenates them.
+//"1" + "2" → "12"
+// console.log(1 +"2");
+// console.log("1"+ 2 + 2);
+
+// console.log(1+2 +"2");
+
+
+/*JavaScript evaluates expressions from left to right.
+
+"1" + 2
+"1" is a string.
+2 becomes "2".
+Result: "12"
+"12" + 2
+"12" is a string.
+2 becomes "2".
+Result: "122"*/
+
+// console.log( (3+ 4) * 5  % 3);
+
+
+// console.log(+true);// 1
+// console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+++gamecounter;
+
+console.log(gamecounter);
+
+
