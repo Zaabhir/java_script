2267e321033551bdbe11de66408450d729e2d055
jai prakash <work.zaabhir089@gmail.com>
2026-07-26 05:19:59 +0000

    string

---
+++ 01_basic/05_string.js
@@ -0,0 +1,47 @@
+const name = "Jp YAdav"
+const repocount  = 58
+
+// console.log(name + repocount + "value");
+
+
+// console.log(`hello my name is ${name} and my repo count is ${repocount}`);
+
+
+const gameName = new String('Jpyadav-jack')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName[0]);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('p'));
+
+
+// const newstring = gameName.substring(0, 4)
+// console.log(newstring);
+
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne =  "    jpking     "
+console.log(newStringOne);
+
+console.log(newStringOne.trim());
+
+const url = "https://jpyadav.com/zaiprakash%22yadava"
+
+url.replace('%22', '-')
+
+console.log(url.replace('%22', '-'));
+console.log(url.includes('jpyadav'));
+console.log(gameName.split('-'));
+
+
+
+
+
+
